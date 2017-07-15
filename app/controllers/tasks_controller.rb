f6787c999c91568e77bc1abbe7890041ce24b561
Wilson Phoong <wphoong@gmail.com>
2017-07-15 04:52:06 -0700

    added feature that allows input form to add task to db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,21 @@ class TasksController < ApplicationController
   def index
     render json: Task.all
   end
+
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    render json: task
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :done)
+  end
 end
